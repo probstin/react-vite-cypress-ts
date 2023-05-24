66d1e5b0a5d959d3a4f40409ad0aa25d832063d6
Probst <probst@probsts-imac.lan>
2023-05-24 18:35:46 -0400

    move filter function outside of jsx

--- src/components/KeyValueList/KeyValueList.tsx
+++ src/components/KeyValueList/KeyValueList.tsx
@@ -2,19 +2,17 @@ import Grid from '@mui/material/Grid'
 import KeyValue from '../KeyValueItem/KeyValueItem'
 
 function KeyValueList({ data }: any) {
-  const keys = Object.keys(data)
+  const keyValues = Object.keys(data).filter(
+    (k) => !(data[k] instanceof Array) && !(data[k] instanceof Object),
+  )
 
   return (
     <Grid container spacing={2}>
-      {keys
-        .filter(
-          (k) => !(data[k] instanceof Array) && !(data[k] instanceof Object),
-        )
-        .map((k, i) => (
-          <Grid item xs={12} sm={6} md={4} key={i}>
-            <KeyValue label={k} value={data[k]} />
-          </Grid>
-        ))}
+      {keyValues.map((k, i) => (
+        <Grid item xs={12} sm={6} md={4} key={i}>
+          <KeyValue label={k} value={data[k]} />
+        </Grid>
+      ))}
     </Grid>
   )
 }
